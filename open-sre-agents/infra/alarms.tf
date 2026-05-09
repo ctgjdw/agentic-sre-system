cabd0183dbd7c0b06b7e5636b12aeec77714bc12
Alex Goh <alexgct@outlook.com>
2026-05-09 13:42:40 +0800

    feat(infra): add sut-db-connection-errors metric filter

--- open-sre-agents/infra/alarms.tf
+++ open-sre-agents/infra/alarms.tf
@@ -8,3 +8,17 @@ resource "aws_cloudwatch_log_group" "ingest_alarm" {
   retention_in_days = 7
   tags              = { Name = "${var.project}-ingest-alarm-logs" }
 }
+
+resource "aws_cloudwatch_log_metric_filter" "db_connection_errors" {
+  name           = "${var.project}-db-connection-errors"
+  log_group_name = aws_cloudwatch_log_group.sut.name
+  pattern        = "?\"could not connect to server\" ?\"connection timeout\" ?\"OperationalError\""
+
+  metric_transformation {
+    name          = "DBConnectionErrors"
+    namespace     = "OpenSRE/SUT"
+    value         = "1"
+    default_value = "0"
+    unit          = "Count"
+  }
+}
